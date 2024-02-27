258ecd2d4650e96354f1dee6d006f1274097c317
yearoftherain <yearoftherain@qq.com>
2024-02-27 15:18:53 +0800

    项目初始化

---
+++ src/main/java/cn/year/coretoolkit/CoreToolkitApplication.java
@@ -0,0 +1,13 @@
+package cn.year.coretoolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreToolkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreToolkitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/year/coretoolkit/CoreToolkitApplicationTests.java
@@ -0,0 +1,15 @@
+package cn.year.coretoolkit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreToolkitApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertNull(null);
+    }
+
+}
